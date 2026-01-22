a67b6eeee36e918f1b43f93e381130d714e1c34a
Elton Jackfama <jackfamaelton@gmail.com>
2026-01-22 13:38:12 +0200

    Add root URL redirect to driver login

--- school_transport/urls.py
+++ school_transport/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path("driver/login/", views.driver_login, name="driver_login"),
     path("driver/logout/", views.driver_logout, name="driver_logout"),
     path("driver/dashboard/", views.driver_dashboard, name="driver_dashboard"),
